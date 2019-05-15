2eba0c3a3e1e2bf27f10ffdfc3039e16e10bf540
cld9x <>
2019-05-16 00:08:45 +0200

    Release Mac dist as .app bundle

---
+++ xbase/tray/icon_darwin.go
@@ -0,0 +1,7 @@
+//+build darwin linux
+
+package tray
+
+import "github.com/cld9x/xbvr/xbase/assets"
+
+var iconData = assets.FileIconsXbvr128Png
---
+++ xbase/tray/icon_windows.go
@@ -0,0 +1,7 @@
+//+build windows
+
+package tray
+
+import "github.com/cld9x/xbvr/xbase/assets"
+
+var iconData []byte = assets.FileIconsXbvrWinIco
