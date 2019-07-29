a3e942c20511f94a4457c5b1ccb4bd44bdb2f32c
cld9x <>
2019-07-29 16:37:33 +0200

    Custom user-agent for imageproxy

--- pkg/xbvr/server.go
+++ pkg/xbvr/server.go
@@ -52,6 +52,7 @@ func StartServer(version, commit, branch, date string) {
 
 	// Imageproxy
 	p := imageproxy.NewProxy(nil, diskCache(filepath.Join(appDir, "imageproxy")))
+	p.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
 	http.Handle("/img/", http.StripPrefix("/img", p))
 
 	// CORS
