f54eec5a3d10a24c593ff010a13d74bdc1794b49
cld9x <>
2019-08-07 17:59:27 +0200

    Loading indicator for file and scene lists

--- ui/src/store/files.js
+++ ui/src/store/files.js
@@ -2,12 +2,15 @@ import ky from "ky";
 
 const state = {
   items: [],
+  isLoading: false,
 };
 
 const actions = {
   load({state}, params) {
+    state.isLoading = true;
     ky.get(`/api/files/list/unmatched`).json().then(data => {
       state.items = data;
+      state.isLoading = false;
     });
   },
 };
