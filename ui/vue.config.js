c2d070b5b6c339f488dd0b9a8c43f6e84fb14744
cld9x <>
2019-09-09 18:37:16 +0200

    Reorganized project structure, added dev notes

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
   publicPath: '/ui',
   outputDir: 'dist',
   lintOnSave: false,
-  // assetsDir: 'static',
+  chainWebpack: config => {
+    config.plugins.delete('progress');
+    config.plugin('simple-progress-webpack-plugin').use(require.resolve('simple-progress-webpack-plugin'), [
+      {
+        format: 'minimal',
+      }
+    ])
+  },
 };
