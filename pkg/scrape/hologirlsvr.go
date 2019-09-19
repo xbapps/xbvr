6feee5f3ffb24e743dbc07ea3907602411d594c7
jrebey <55519905+jrebey@users.noreply.github.com>
2019-09-18 21:58:17 -0400

    Add HoloGirlsVR scraper

---
+++ pkg/scrape/hologirlsvr.go
@@ -0,0 +1,102 @@
+package scrape
+
+import (
+        "log"
+        "regexp"
+        "strings"
+
+        "github.com/gocolly/colly"
+        "github.com/mozillazg/go-slugify"
+        "github.com/thoas/go-funk"
+)
+
+func ScrapeHoloGirlsVR(knownScenes []string, out *[]ScrapedScene) error {
+        siteCollector := colly.NewCollector(
+                colly.AllowedDomains("www.hologirlsvr.com"),
+                colly.CacheDir(siteCacheDir),
+                colly.UserAgent(userAgent),
+        )
+
+        sceneCollector := colly.NewCollector(
+                colly.AllowedDomains("www.hologirlsvr.com"),
+                colly.CacheDir(sceneCacheDir),
+                colly.UserAgent(userAgent),
+        )
+
+        siteCollector.OnRequest(func(r *colly.Request) {
+                log.Println("visiting", r.URL.String())
+        })
+
+        sceneCollector.OnRequest(func(r *colly.Request) {
+                log.Println("visiting", r.URL.String())
+        })
+
+        sceneCollector.OnHTML(`html`, func(e *colly.HTMLElement) {
+                sc := ScrapedScene{}
+                sc.SceneType = "VR"
+                sc.Studio = "HoloFilm Productions"
+                sc.Site = "HoloGirlsVR"
+                sc.HomepageURL = strings.Split(e.Request.URL.String(), "?")[0]
+
+                // Scene ID - get from URL
+                tmp := strings.Split(sc.HomepageURL, "/")
+                sc.SiteID = tmp[len(tmp)-1]
+                sc.SceneID = slugify.Slugify(sc.Site) + "-" + sc.SiteID
+
+                // Title
+                e.ForEach(`div.video-title h3`, func(id int, e *colly.HTMLElement) {
+                        if id == 0 {
+                                sc.Title = e.Text
+                        }
+                })
+
+                // Cover URLs
+                e.ForEach(`div.vidCover`, func(id int, e *colly.HTMLElement) {
+                        if id == 0 {
+                                sc.Covers = append(sc.Covers, strings.TrimSpace(e.ChildAttr(`img`, "src")))
+                        }
+                })
+
+                // Gallery
+                e.ForEach(`div.vid-flex-container a`, func(id int, e *colly.HTMLElement) {
+                        sc.Gallery = append(sc.Gallery, strings.TrimSpace(e.Attr("href")))
+                })
+
+                // Synopsis
+                r, _ := regexp.Compile("(?s)Synopsis:(.*)Tags:")
+                e.ForEach(`div.vidpage-info`, func(id int, e *colly.HTMLElement) {
+                        if id == 0 {
+                                match := r.FindStringSubmatch(e.Text)
+                                sc.Synopsis = strings.TrimSpace(match[1])
+                        }
+                })
+
+                // Cast
+                e.ForEach(`div.vidpage-featuring span`, func(id int, e *colly.HTMLElement) {
+                        sc.Cast = append(sc.Cast, strings.TrimSpace(e.Text))
+                })
+
+                // Tags
+                e.ForEach(`div.videopage-tags em`, func(id int, e *colly.HTMLElement) {
+                        sc.Tags = append(sc.Tags, strings.TrimSpace(e.Text))
+                })
+
+                *out = append(*out, sc)
+        })
+
+        siteCollector.OnHTML(`div.pagination-container li a`, func(e *colly.HTMLElement) {
+                pageURL := e.Request.AbsoluteURL(e.Attr("href"))
+                siteCollector.Visit(pageURL)
+        })
+
+        siteCollector.OnHTML(`div.memVid div.coverPrev a`, func(e *colly.HTMLElement) {
+                sceneURL := e.Request.AbsoluteURL(e.Attr("href"))
+
+                // If scene exist in database, there's no need to scrape
+                if !funk.ContainsString(knownScenes, sceneURL) && !strings.Contains(sceneURL, "/join") {
+                        sceneCollector.Visit(sceneURL)
+                }
+        })
+
+        return siteCollector.Visit("https://www.hologirlsvr.com/Scenes")
+}
