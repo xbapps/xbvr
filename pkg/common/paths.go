c5e7f227279cca55c92ba7870baac5eee2958919
cld9x <>
2019-11-05 22:47:40 +0100

    Since resulting index is now thinner, remove previous one on application start

--- pkg/common/paths.go
+++ pkg/common/paths.go
@@ -12,7 +12,8 @@ var AppDir string
 var ImgDir string
 var CacheDir string
 var BinDir string
-var IndexDir string
+var IndexDirV1 string
+var IndexDirV2 string
 
 func InitPaths() {
 	AppDir = appdir.New("xbvr").UserConfig()
@@ -20,11 +21,17 @@ func InitPaths() {
 	ImgDir = filepath.Join(AppDir, "imageproxy")
 	CacheDir = filepath.Join(AppDir, "cache")
 	BinDir = filepath.Join(AppDir, "bin")
-	IndexDir = filepath.Join(AppDir, "search")
+	IndexDirV1 = filepath.Join(AppDir, "search")
+	IndexDirV2 = filepath.Join(AppDir, "search-v2")
+
+	// Remove search index v1
+	if _, err := os.Stat(IndexDirV1); !os.IsNotExist(err) {
+		os.RemoveAll(IndexDirV1)
+	}
 
 	_ = os.MkdirAll(AppDir, os.ModePerm)
 	_ = os.MkdirAll(ImgDir, os.ModePerm)
 	_ = os.MkdirAll(CacheDir, os.ModePerm)
 	_ = os.MkdirAll(BinDir, os.ModePerm)
-	_ = os.MkdirAll(IndexDir, os.ModePerm)
+	_ = os.MkdirAll(IndexDirV2, os.ModePerm)
 }
