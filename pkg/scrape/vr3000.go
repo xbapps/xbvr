1f5b41c4b7575bdd32b02d3c51245d7457452767
jrebey <55519905+jrebey@users.noreply.github.com>
2019-11-11 00:12:49 +0000

    Don't add scene if already scraped

--- pkg/scrape/vr3000.go
+++ pkg/scrape/vr3000.go
@@ -100,11 +100,10 @@ func VR3000(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<
 			sceneURL := e.Request.AbsoluteURL(e.ChildAttr(`div.welldescription a.btn-primary`, "href"))
 			if !funk.ContainsString(knownScenes, sceneURL) && !strings.Contains(sceneURL, "/join") {
 				sc.HomepageURL = sceneURL
+				if sc.Title != "" {
+					out <- sc
+				}
 			}
-		}
-
-		if sc.Title != "" {
-			out <- sc
 		}
 	})
 
