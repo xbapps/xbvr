e376936fe3331bf6793d52fa14c6435759503e69
jrebey <55519905+jrebey@users.noreply.github.com>
2019-11-13 16:43:35 +0000

    Merge upstream

--- .drone.dockerfile
+++ .drone.dockerfile
@@ -4,10 +4,10 @@ RUN apt update && apt install -y wget ca-certificates
 ARG DRONE_TAG
 
 RUN wget -O /tmp/xbvr.tgz "https://github.com/xbapps/xbvr/releases/download/"$DRONE_TAG"/xbvr_"$DRONE_TAG"_Linux_x86_64.tar.gz" && \
-    tar xvfz /tmp/xbvr.tgz -C /usr/local/bin/ && \
-    rm /tmp/xbvr.tgz
+  tar xvfz /tmp/xbvr.tgz -C /usr/local/bin/ && \
+  rm /tmp/xbvr.tgz
 
-FROM gcr.io/distroless/base
+FROM gcr.io/distroless/base-debian10
 COPY --from=temp /usr/local/bin/xbvr /
 
 EXPOSE 9998-9999
--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -3,16 +3,20 @@ package scrape
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/ProtonMail/go-appdir"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 	"github.com/sirupsen/logrus"
 	"github.com/xbapps/xbvr/pkg/common"
 	"github.com/xbapps/xbvr/pkg/models"
+	"golang.org/x/net/html"
 )
 
 var log = &common.Log
@@ -23,7 +27,6 @@ var siteCacheDir string
 var sceneCacheDir string
 
 var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
-
 
 func createCollector(domains ...string) *colly.Collector {
 	c := colly.NewCollector(
@@ -117,6 +120,43 @@ func updateSiteLastUpdate(id string) {
 	}
 	site.LastUpdate = time.Now()
 	site.Save()
+}
+
+func traverseNodes(node *html.Node, fn func(*html.Node)) {
+	if node == nil {
+		return
+	}
+
+	fn(node)
+
+	for cur := node.FirstChild; cur != nil; cur = cur.NextSibling {
+		traverseNodes(cur, fn)
+	}
+}
+
+func findComments(sel *goquery.Selection) []string {
+	comments := []string{}
+	for _, node := range sel.Nodes {
+		traverseNodes(node, func(node *html.Node) {
+			if node.Type == html.CommentNode {
+				comments = append(comments, node.Data)
+			}
+		})
+	}
+	return comments
+}
+
+func getFilenameFromURL(u string) string {
+	p, _ := url.Parse(u)
+	return path.Base(p.Path)
+}
+
+func getTextFromHTMLWithSelector(data string, sel string) string {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(doc.Find(sel).Text())
 }
 
 func init() {
