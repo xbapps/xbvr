ea005d1599033cb9411e5a6c96d0929c520cf856
cld9x <>
2019-11-30 00:31:57 +0100

    Disable soft-delete for Actor model

--- pkg/models/model_actor.go
+++ pkg/models/model_actor.go
@@ -8,7 +8,6 @@ type Actor struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"-"`
 	UpdatedAt time.Time  `json:"-"`
-	DeletedAt *time.Time `sql:"index" json:"-"`
 
 	Name   string  `gorm:"unique_index" json:"name"`
 	Scenes []Scene `gorm:"many2many:scene_cast;" json:"-"`
