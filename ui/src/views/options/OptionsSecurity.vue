c7027684f5794051a0342ec9dea28b2169fabcbb
jrebey <55519905+jrebey@users.noreply.github.com>
2019-11-29 20:00:49 -0500

    Allow DLNA to be enabled/disabled in the UI (#228)

---
+++ ui/src/views/options/OptionsSecurity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="content">
+      <h3>{{$t("Enable DLNA")}}</h3>
+      <p>
+        {{$t("You can enable/disable DLNA access")}}
+      </p>
+      <div class="switch">
+        <b-switch :value="dlnaOptions.dlna_enabled" @input="$store.dispatch('optionsSecurity/enableDLNA', !dlnaOptions.dlna_enabled)"/>
+      </div>
+      <hr/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ky from "ky";
+
+  export default {
+    name: "OptionsSecurity",
+    mounted() {
+      this.$store.dispatch("optionsSecurity/load");
+    },
+    computed: {
+      dlnaOptions: function () {
+        return this.$store.state.optionsSecurity.dlna_options;
+      },
+    },
+  }
+</script>
