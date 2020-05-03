c93575a6b4ce1a328882a3ec40ea4db9d03692a6
cld9x <>
2020-05-04 00:32:31 +0200

    Refactored data access for DLNA

---
+++ pkg/models/utils.go
@@ -0,0 +1,71 @@
+package models
+
+type DMSData struct {
+	Sites        []string `json:"sites"`
+	Actors       []string `json:"actors"`
+	Tags         []string `json:"tags"`
+	ReleaseGroup []string `json:"release_group"`
+	Volumes      []Volume `json:"volumes"`
+}
+
+func GetDMSData() DMSData {
+	db, _ := GetDB()
+	defer db.Close()
+
+	// Get all accessible scenes
+	var scenes []Scene
+	tx := db.
+		Model(&scenes).
+		Preload("Cast").
+		Preload("Tags").
+		Preload("Files")
+
+	tx = tx.Where("is_accessible = ?", 1)
+
+	// Available sites
+	tx.Group("site").Find(&scenes)
+	var outSites []string
+	for i := range scenes {
+		if scenes[i].Site != "" {
+			outSites = append(outSites, scenes[i].Site)
+		}
+	}
+
+	// Available release dates (YYYY-MM)
+	tx.Select("strftime('%Y-%m', release_date) as release_date_text").
+		Group("strftime('%Y-%m', release_date)").Find(&scenes)
+	var outRelease []string
+	for i := range scenes {
+		outRelease = append(outRelease, scenes[i].ReleaseDateText)
+	}
+
+	// Available tags
+	tx.Joins("left join scene_tags on scene_tags.scene_id=scenes.id").
+		Joins("left join tags on tags.id=scene_tags.tag_id").
+		Group("tags.name").Select("tags.name as release_date_text").Find(&scenes)
+
+	var outTags []string
+	for i := range scenes {
+		if scenes[i].ReleaseDateText != "" {
+			outTags = append(outTags, scenes[i].ReleaseDateText)
+		}
+	}
+
+	// Available actors
+	tx.Joins("left join scene_cast on scene_cast.scene_id=scenes.id").
+		Joins("left join actors on actors.id=scene_cast.actor_id").
+		Group("actors.name").Select("actors.name as release_date_text").Find(&scenes)
+
+	var outCast []string
+	for i := range scenes {
+		if scenes[i].ReleaseDateText != "" {
+			outCast = append(outCast, scenes[i].ReleaseDateText)
+		}
+	}
+
+	// Available volumes
+	var vol []Volume
+	db.Where("is_available = ?", true).Find(&vol)
+
+	return DMSData{Sites: outSites, Tags: outTags, Actors: outCast, Volumes: vol, ReleaseGroup: outRelease}
+}
--- pkg/xbvr/api_dms.go
+++ pkg/xbvr/api_dms.go
@@ -15,14 +15,6 @@ import (
 	"github.com/xbapps/xbvr/pkg/models"
 )
 
-type DMSDataResponse struct {
-	Sites        []string        `json:"sites"`
-	Actors       []string        `json:"actors"`
-	Tags         []string        `json:"tags"`
-	ReleaseGroup []string        `json:"release_group"`
-	Volumes      []models.Volume `json:"volumes"`
-}
-
 type DMSResource struct{}
 
 var (
@@ -41,15 +33,6 @@ func (i DMSResource) WebService() *restful.WebService {
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
-	ws.Route(ws.GET("/base").To(i.base).
-		Metadata(restfulspec.KeyOpenAPITags, tags))
-
-	ws.Route(ws.GET("/scene").To(i.sceneById).
-		Metadata(restfulspec.KeyOpenAPITags, tags))
-
-	ws.Route(ws.GET("/file").To(i.fileById).
-		Metadata(restfulspec.KeyOpenAPITags, tags))
-
 	ws.Route(ws.GET("/file/{file-id}").To(i.getFile).
 		Param(ws.PathParameter("file-id", "File ID").DataType("int")).
 		Metadata(restfulspec.KeyOpenAPITags, tags))
@@ -59,98 +42,6 @@ func (i DMSResource) WebService() *restful.WebService {
 		Metadata(restfulspec.KeyOpenAPITags, tags))
 
 	return ws
-}
-
-func (i DMSResource) sceneById(req *restful.Request, resp *restful.Response) {
-	sceneId := req.QueryParameter("id")
-
-	db, _ := models.GetDB()
-	defer db.Close()
-
-	var scene models.Scene
-	db.Preload("Cast").
-		Preload("Tags").
-		Preload("Files").
-		Where(&models.Scene{SceneID: sceneId}).First(&scene)
-
-	resp.WriteHeaderAndEntity(http.StatusOK, scene)
-}
-
-func (i DMSResource) fileById(req *restful.Request, resp *restful.Response) {
-	fileId, err := strconv.Atoi(req.QueryParameter("id"))
-	if err != nil {
-		return
-	}
-
-	db, _ := models.GetDB()
-	defer db.Close()
-
-	var file models.File
-	db.Where(&models.File{ID: uint(fileId)}).First(&file)
-
-	resp.WriteHeaderAndEntity(http.StatusOK, file)
-}
-
-func (i DMSResource) base(req *restful.Request, resp *restful.Response) {
-	db, _ := models.GetDB()
-	defer db.Close()
-
-	// Get all accessible scenes
-	var scenes []models.Scene
-	tx := db.
-		Model(&scenes).
-		Preload("Cast").
-		Preload("Tags").
-		Preload("Files")
-
-	tx = tx.Where("is_accessible = ?", 1)
-
-	// Available sites
-	tx.Group("site").Find(&scenes)
-	var outSites []string
-	for i := range scenes {
-		if scenes[i].Site != "" {
-			outSites = append(outSites, scenes[i].Site)
-		}
-	}
-
-	// Available release dates (YYYY-MM)
-	tx.Select("strftime('%Y-%m', release_date) as release_date_text").
-		Group("strftime('%Y-%m', release_date)").Find(&scenes)
-	var outRelease []string
-	for i := range scenes {
-		outRelease = append(outRelease, scenes[i].ReleaseDateText)
-	}
-
-	// Available tags
-	tx.Joins("left join scene_tags on scene_tags.scene_id=scenes.id").
-		Joins("left join tags on tags.id=scene_tags.tag_id").
-		Group("tags.name").Select("tags.name as release_date_text").Find(&scenes)
-
-	var outTags []string
-	for i := range scenes {
-		if scenes[i].ReleaseDateText != "" {
-			outTags = append(outTags, scenes[i].ReleaseDateText)
-		}
-	}
-
-	// Available actors
-	tx.Joins("left join scene_cast on scene_cast.scene_id=scenes.id").
-		Joins("left join actors on actors.id=scene_cast.actor_id").
-		Group("actors.name").Select("actors.name as release_date_text").Find(&scenes)
-
-	var outCast []string
-	for i := range scenes {
-		if scenes[i].ReleaseDateText != "" {
-			outCast = append(outCast, scenes[i].ReleaseDateText)
-		}
-	}
-
-	// Available volumes
-	var vol []models.Volume
-	db.Where("is_available = ?", true).Find(&vol)
-
-	resp.WriteHeaderAndEntity(http.StatusOK, DMSDataResponse{Sites: outSites, Tags: outTags, Actors: outCast, Volumes: vol, ReleaseGroup: outRelease})
 }
 
 func (i DMSResource) getPreview(req *restful.Request, resp *restful.Response) {
