28beac87013cc83891cfa3e16e1830bb301cfd56
jrebey <55519905+jrebey@users.noreply.github.com>
2020-05-12 04:10:30 +0000

    Move db lock functions out of model_kv

--- pkg/models/db.go
+++ pkg/models/db.go
@@ -23,6 +23,36 @@ func GetDB() (*gorm.DB, error) {
 	return db, nil
 }
 
+// Lock functions
+
+func CreateLock(lock string) {
+	obj := KV{Key: "lock-" + lock, Value: "1"}
+	obj.Save()
+
+	common.PublishWS("lock.change", map[string]interface{}{"name": lock, "locked": true})
+}
+
+func CheckLock(lock string) bool {
+	db, _ := GetDB()
+	defer db.Close()
+
+	var obj KV
+	err := db.Where(&KV{Key: "lock-" + lock}).First(&obj).Error
+	if err == nil {
+		return true
+	}
+	return false
+}
+
+func RemoveLock(lock string) {
+	db, _ := GetDB()
+	defer db.Close()
+
+	db.Where("key = ?", "lock-"+lock).Delete(&KV{})
+
+	common.PublishWS("lock.change", map[string]interface{}{"name": lock, "locked": false})
+}
+
 func init() {
 	common.InitPaths()
 }
