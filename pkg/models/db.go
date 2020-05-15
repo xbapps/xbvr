aad47256642ac8b57e793db15368c622b8ab9b43
jrebey <55519905+jrebey@users.noreply.github.com>
2020-05-15 14:02:03 -0400

    Implement retry logic to fix sqlite3 locking issue

--- pkg/models/db.go
+++ pkg/models/db.go
@@ -3,6 +3,7 @@ package models
 import (
 	"path/filepath"
 
+	"github.com/avast/retry-go"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	_ "github.com/mattn/go-sqlite3"
@@ -16,10 +17,23 @@ func GetDB() (*gorm.DB, error) {
 		log.Debug("Getting DB handle from ", common.GetCallerFunctionName())
 	}
 
-	db, err := gorm.Open("sqlite3", "file:"+filepath.Join(common.AppDir, "main.db")+"?"+common.SQLITE_PARAMS)
+	var db *gorm.DB
+	var err error
+
+	err = retry.Do(
+		func() error {
+			db, err = gorm.Open("sqlite3", "file:"+filepath.Join(common.AppDir, "main.db")+"?"+common.SQLITE_PARAMS)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	)
+
 	if err != nil {
-		log.Fatal("failed to connect database", err)
+		log.Fatal("Failed to connect to database ", err)
 	}
+
 	return db, nil
 }
 
