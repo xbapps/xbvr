8a346df8dea8851a278aa465b99cfaf47e9ca84c
SpedNSFW <spedwards.adult@gmail.com>
2020-07-27 08:33:42 +1000

    add scroll to top button

--- ui/src/views/scenes/Scenes.vue
+++ ui/src/views/scenes/Scenes.vue
@@ -4,6 +4,8 @@
 
       <div class="column is-one-fifth">
         <Filters/>
+
+        <a id="toTop">Top</a>
       </div>
 
       <List/>
@@ -21,6 +23,25 @@
   export default {
     name: "Scenes",
     components: {Filters, List, Details},
+    mounted() {
+      const toTop = document.getElementById('toTop');
+      addEventListener('scroll', function() {
+        toTop.style.display = document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+          ? 'block'
+          : 'none';
+      });
+      toTop.addEventListener('click', function() {
+        scrollToTop();
+      });
+
+      const scrollToTop = () => {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+          window.requestAnimationFrame(scrollToTop);
+          window.scrollTo(0, c - c / 16);
+        }
+      };
+    },
     beforeRouteEnter(to, from, next) {
       next(vm => {
         if (to.query !== undefined) {
@@ -43,3 +64,21 @@
     }
   }
 </script>
+
+<style scoped>
+  #toTop {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    left: 30px;
+    background-color: #f0f0f0;
+    color: #4a4a4a;
+    padding: 15px;
+    border-radius: 10px;
+    font-size: 18px;
+  }
+
+  #toTop:hover {
+    background-color: #BDBDBD;
+  }
+</style>
