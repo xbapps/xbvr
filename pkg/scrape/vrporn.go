03a433008c0269fe8202c770fb816e88a5ea392f
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2020-08-10 05:11:12 -0400

    Added Tonight's Girlfriend VR scraper (#332)

--- pkg/scrape/vrporn.go
+++ pkg/scrape/vrporn.go
@@ -138,12 +138,17 @@ func VRPorn(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<
 
 // RandysRoadStop - Has its own site but does not include info such as tags: https://randysroadstop.com
 func RandysRoadStop(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene) error {
-	return VRPorn(wg, updateSite, knownScenes, out, "randysroadstop", "Randys Road Stop", "NaughtyAmerica")
+	return VRPorn(wg, updateSite, knownScenes, out, "randysroadstop", "Randy's Road Stop", "NaughtyAmerica")
 }
 
 // RealTeensVR - Has its own site but contains no scene info: https://realteensvr.com
 func RealTeensVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene) error {
 	return VRPorn(wg, updateSite, knownScenes, out, "realteensvr", "Real Teens VR", "NaughtyAmerica")
+}
+
+// TonightsGirlfriend - Has its own site but no tags or previews. : https://tonightsgirlfriend.com
+func TonightsGirlfriend(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene) error {
+	return VRPorn(wg, updateSite, knownScenes, out, "tonightsgirlfriendvr", "Tonight's Girlfriend VR", "NaughtyAmerica")
 }
 
 // VRClubz - Has its own site but contains less scenes than VRPorn?: https://vrclubz.com
@@ -154,5 +159,6 @@ func VRClubz(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan
 func init() {
 	registerScraper("randysroadstop", "Randys Road Stop (VRPorn)", "https://mcdn.vrporn.com/files/20170718073527/randysroadstop-vr-porn-studio-vrporn.com-virtual-reality.png", RandysRoadStop)
 	registerScraper("realteensvr", "Real Teens VR (VRPorn)", "https://mcdn.vrporn.com/files/20170718063811/realteensvr-vr-porn-studio-vrporn.com-virtual-reality.png", RealTeensVR)
+	registerScraper("tonightsgirlfriend", "Tonight's Girlfriend VR (VRPorn)", "https://mcdn.vrporn.com/files/20200404124349/TNGF_LOGO_BLK.jpg", TonightsGirlfriend)
 	registerScraper("vrclubz", "VRClubz (VRPorn)", "https://mcdn.vrporn.com/files/20200421094123/vrclubz_logo_NEW-400x400_webwhite.png", VRClubz)
 }
