78c32c3254885dd2c555ec3eaed2a7926538f015
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2020-12-01 00:12:49 -0500

    Update virtualrealporn.go
    put "span" back in tag routine to be consistent. didn't see any difference.

--- pkg/scrape/virtualrealporn.go
+++ pkg/scrape/virtualrealporn.go
@@ -64,7 +64,7 @@ func VirtualRealPornSite(wg *sync.WaitGroup, updateSite bool, knownScenes []stri
 		})
 
 		// Tags
-		e.ForEach(`a[href*="/tag/"]`, func(id int, e *colly.HTMLElement) {
+		e.ForEach(`a[href*="/tag/"] span`, func(id int, e *colly.HTMLElement) {
 			sc.Tags = append(sc.Tags, strings.TrimSpace(e.Text))
 		})
 		if scraperID == "virtualrealgay" {
