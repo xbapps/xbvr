c59edea271e77c1766da705d0acf251819bfa557
lockedmouse <51096617+lockedmouse@users.noreply.github.com>
2021-03-31 15:17:14 +0200

    Add "tb" as common word

--- ui/src/views/files/SceneMatch.vue
+++ ui/src/views/files/SceneMatch.vue
@@ -90,7 +90,7 @@ export default {
         '180', '180x180', '30fps', '360', '3dh', '4k', '5k', '60fps', '6k', '7k', '8k',
         'funscript', 'h264', 'h265', 'hevc', 'hq', 'lq', 'lr', 'mkv', 'mkx200', 'mkx220',
         'mono', 'mp4', 'oculus', 'oculus5k', 'oculusrift', 'original', 'smartphone',
-        'vrca220', 'vp9'
+        'tb', 'vrca220', 'vp9'
       ]
       const isNotCommonWord = word => !commonWords.includes(word.toLowerCase()) && !/^[0-9]+p$/.test(word)
 
