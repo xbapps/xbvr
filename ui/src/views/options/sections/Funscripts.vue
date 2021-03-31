69f4ea1f06c497d2d12c9663e13b45fe72c2319c
crwxaj <52156245+crwxaj@users.noreply.github.com>
2021-03-31 20:14:10 +0200

    Update ui/src/views/options/sections/Funscripts.vue
    Co-authored-by: lockedmouse <51096617+lockedmouse@users.noreply.github.com>

--- ui/src/views/options/sections/Funscripts.vue
+++ ui/src/views/options/sections/Funscripts.vue
@@ -4,16 +4,16 @@
       
       <h3>{{$t('Export funscripts')}}</h3>
       <p>
-        {{$t('Here you can download a zip file with a funscript for each scripted scene. The files are named with the scene title and the scene id. Scipted scenes in the DeoVR interface will be named accordingly. For scenes with multiple scripts you can choose a preferred one in the scene details view. Otherwise the last one added is chosen.')}}
+        {{$t('Here you can download a ZIP file containing a funscript for each scripted scene. The files are named by scene title and scene id, just like they appear in the DeoVR interface. If a scene has multiple scripts you can choose a preferred script in the scene details view. Otherwise, the last added script is chosen.')}}
       </p>
       <p>
-        {{$t('This export won\'t work with DLNA.')}}
+        {{$t('Note that the filenames are not compatible with DLNA.')}}
       </p>
       <p>
         {{$t('To use this export with DeoVR: Unzip and put the files in the Interactive folder on your device.')}}
       </p>
       <p>
-        {{$t('To use this export with ScriptPlayer: Unzip and put the files in a folder of your choice. Add this folder in the Paths section in the settings. Connect to DeoVR.')}}
+        {{$t('To use this export with ScriptPlayer: Unzip and put the files in a folder of your choice. In the ScriptPlayer settings, add this folder in the Paths section, then connect to DeoVR.')}}
       </p>
       <b-button type="is-primary" @click="exportFunscripts" :disabled="count === 0">{{$t('Download funscripts for DeoVR')}}</b-button>
       <p>
