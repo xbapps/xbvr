88a00d048c4b1b4e042e1644590b53d262a5d07b
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2021-04-16 07:31:00 -0400

    perVRt
    site name change to fix automatching. will also rename existing "perVRt/Terrible"

--- pkg/migrations/migrations.go
+++ pkg/migrations/migrations.go
@@ -414,6 +414,15 @@ func Migrate() {
 				return tx.AutoMigrate(File{}).Error
 			},
 		},
+		{
+			// perVRt change siteID
+			ID: "0024-revert-pervrt",
+			Migrate: func(tx *gorm.DB) error {
+				var scenes []models.Scene
+				return db.Model(&scenes).Where("site = ?", "perVRt/Terrible").Update("site", "perVRt").Error
+
+			},
+		},
 	})
 
 	if err := m.Migrate(); err != nil {
