e3ea460e8a87a5894ff817399c965339fc162016
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2021-05-20 18:37:06 -0400

    Update caribbeancom.go
    temporary revert to original "single page" code. The pagination code on the website is broken.

--- pkg/scrape/caribbeancom.go
+++ pkg/scrape/caribbeancom.go
@@ -116,10 +116,11 @@ func CariVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<
 		}
 	})
 
-	siteCollector.OnHTML(`.pagination-large .pagination__item[rel="next"]`, func(e *colly.HTMLElement) {
-		pageURL := e.Request.AbsoluteURL(e.Attr("href"))
-		siteCollector.Visit(pageURL)
-	})
+	/*	siteCollector.OnHTML(`.pagination-large .pagination__item[rel="next"]`, func(e *colly.HTMLElement) {
+			pageURL := e.Request.AbsoluteURL(e.Attr("href"))
+			siteCollector.Visit(pageURL)
+		})
+	*/
 	siteCollector.Visit("https://en.caribbeancom.com/eng/listpages/vr1.htm")
 
 	if updateSite {
