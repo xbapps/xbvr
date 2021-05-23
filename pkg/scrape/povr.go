33ad7e9d59349e3ecf8ca869f8d097170d522511
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2021-05-23 07:32:10 -0400

    POVR scraper fixes (#519)

--- pkg/scrape/povr.go
+++ pkg/scrape/povr.go
@@ -32,24 +32,22 @@ func POVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<- 
 		sc.SceneID = slugify.Slugify(scraperID) + "-" + sc.SiteID
 
 		// Title
-		e.ForEach(`h1.player__title`, func(id int, e *colly.HTMLElement) {
-			sc.Title = e.Text
+		e.ForEach(`h4.title`, func(id int, e *colly.HTMLElement) {
+			// 7K Maid For Play
+			sc.Title = strings.Split(e.Text, "K")[1]
 		})
 
-		// Date
-		e.ForEach(`div.player__meta div.player__meta-item`, func(id int, e *colly.HTMLElement) {
+		// Date & Duration
+		e.ForEach(`div.video__details-grid p.player__date`, func(id int, e *colly.HTMLElement) {
+			tmpDetails := strings.Split(e.Text, "  •  ")
 
-			if id == 2 {
-				tmpDate, _ := goment.New(e.Text, "DD MMMM, YYYY")
-				sc.Released = tmpDate.Format("YYYY-MM-DD")
-			}
+			// Date
+			tmpDate, _ := goment.New(tmpDetails[1], "DD MMMM, YYYY")
+			sc.Released = tmpDate.Format("YYYY-MM-DD")
 			// Duration
-			//		e.ForEach(`div.detail__date_time span.time`, func(id int, e *colly.HTMLElement) {
-			if id == 1 {
-				tmpDuration, err := strconv.Atoi(strings.TrimSpace(strings.Replace(e.Text, "min,", "", -1)))
-				if err == nil {
-					sc.Duration = tmpDuration
-				}
+			tmpDuration, err := strconv.Atoi(strings.TrimSpace(strings.Replace(tmpDetails[0], "min", "", -1)))
+			if err == nil {
+				sc.Duration = tmpDuration
 			}
 		})
 
@@ -68,37 +66,25 @@ func POVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<- 
 			}
 		})
 
-		// Gallery
-		//		size := "1024"
-		//		for _, x := range []string{"1", "2", "3", "4", "5", "6"} {
-		//			tmpGallery := "https://cdns-i." + scraperID + ".com/" + sc.SiteID[0:1] + "/" + sc.SiteID[0:4] + "/" + sc.SiteID + "/thumbs/" + size + "_" + x + ".jpg"
-		//			sc.Gallery = append(sc.Gallery, tmpGallery)
-		//		}
-
 		// Synopsis
-		e.ForEach(`p.player-link__description`, func(id int, e *colly.HTMLElement) {
+		e.ForEach(`div.player__description`, func(id int, e *colly.HTMLElement) {
 			sc.Synopsis = strings.TrimSpace(e.Text)
 		})
 
 		// Tags
-		e.ForEach(`a.player-link__tag`, func(id int, e *colly.HTMLElement) {
+		e.ForEach(`a.btn--default`, func(id int, e *colly.HTMLElement) {
 			sc.Tags = append(sc.Tags, e.Text)
 		})
 
 		// Cast
-		e.ForEach(`a.player-link__actor`, func(id int, e *colly.HTMLElement) {
+		e.ForEach(`a.btn--eptenary`, func(id int, e *colly.HTMLElement) {
 			sc.Cast = append(sc.Cast, strings.TrimSpace(e.Text))
 		})
 
 		out <- sc
 	})
 
-	//	siteCollector.OnHTML(`div.teaser-video a.teaser-video__main`, func(e *colly.HTMLElement) {
-	//		pageURL := e.Request.AbsoluteURL(e.Attr("href"))
-	//		siteCollector.Visit(pageURL)
-	//	})
-
-	siteCollector.OnHTML(`div.teaser-video a.teaser-video__main`, func(e *colly.HTMLElement) {
+	siteCollector.OnHTML(`div.thumbnail-wrap div.thumbnail a.thumbnail__link`, func(e *colly.HTMLElement) {
 		sceneURL := e.Request.AbsoluteURL(e.Attr("href"))
 
 		// If scene exist in database, there's no need to scrape
