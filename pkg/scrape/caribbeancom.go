ca65a6a983beda72e8a4437af3cb8006fd74e454
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2021-05-23 07:37:11 -0400

    Fixed Caribbeancom scraper (#520)
    * Update caribbeancom.go
    
    temporary revert to original "single page" code. The pagination code on the website is broken.
    
    * Update caribbeancom.go
    
    fixes broken pagination for them.

--- pkg/scrape/caribbeancom.go
+++ pkg/scrape/caribbeancom.go
@@ -117,9 +117,11 @@ func CariVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out chan<
 	})
 
 	siteCollector.OnHTML(`.pagination-large .pagination__item[rel="next"]`, func(e *colly.HTMLElement) {
-		pageURL := e.Request.AbsoluteURL(e.Attr("href"))
+		// replace "all" with "vr" to allow for correct page navigation
+		pageURL := strings.Replace(e.Request.AbsoluteURL(e.Attr("href")), "all", "vr", 1)
 		siteCollector.Visit(pageURL)
 	})
+
 	siteCollector.Visit("https://en.caribbeancom.com/eng/listpages/vr1.htm")
 
 	if updateSite {
