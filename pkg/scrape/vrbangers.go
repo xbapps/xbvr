58a65c04afa3bb10e49678b62e80a8ed16eafe98
crwxaj <52156245+crwxaj@users.noreply.github.com>
2021-05-23 13:33:56 +0200

    Prevent crash in VRBangers scraper (#506)
    * prevent crash in VRBangers scraper
    
    * Update pkg/scrape/vrbangers.go
    
    Co-authored-by: lockedmouse <51096617+lockedmouse@users.noreply.github.com>
    
    Co-authored-by: crwxaj <crwxaj>
    Co-authored-by: lockedmouse <51096617+lockedmouse@users.noreply.github.com>

--- pkg/scrape/vrbangers.go
+++ pkg/scrape/vrbangers.go
@@ -28,6 +28,10 @@ func VRBangersSite(wg *sync.WaitGroup, updateSite bool, knownScenes []string, ou
 		sc.Site = siteID
 		sc.HomepageURL = strings.Split(e.Request.URL.String(), "?")[0]
 
+		if e.ChildAttr(`link[rel=shortlink]`, "href") == "" {
+			log.Printf("Skipping %s because it is not a valid scene.", e.Request.URL.String())
+			return
+		}
 		// Scene ID - get from URL
 		sc.SiteID = strings.Split(e.ChildAttr(`link[rel=shortlink]`, "href"), "?p=")[1]
 		sc.SceneID = slugify.Slugify(sc.Site) + "-" + sc.SiteID
