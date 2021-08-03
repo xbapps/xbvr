8eee1be3f0f26e5cfd0e50ab2b6739c390218d0c
lockedmouse <51096617+lockedmouse@users.noreply.github.com>
2021-08-03 14:26:46 +0200

    Add 3dv to list of common words

--- ui/src/views/files/SceneMatch.vue
+++ ui/src/views/files/SceneMatch.vue
@@ -89,8 +89,8 @@ export default {
   methods: {
     initView () {
       const commonWords = [
-        '180', '180x180', '30fps', '30m', '360', '3dh', '4k', '5k', '60fps', '6k', '7k',
-        '8k', 'fb360', 'funscript', 'h264', 'h265', 'hevc', 'hq', 'lq', 'lr', 'mkv',
+        '180', '180x180', '30fps', '30m', '360', '3dh', '3dv', '4k', '5k', '60fps', '6k',
+        '7k', '8k', 'fb360', 'funscript', 'h264', 'h265', 'hevc', 'hq', 'lq', 'lr', 'mkv',
         'mkx200', 'mkx220', 'mono', 'mp4', 'oculus', 'oculus5k', 'oculusrift', 'original',
         'smartphone', 'tb', 'vrca220', 'vp9'
       ]
