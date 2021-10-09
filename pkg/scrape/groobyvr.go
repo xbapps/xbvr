e09ebac2bfef4574eac7cc27cdb1da0c587e4c14
ZoroSenku <92191932+ZoroSenku@users.noreply.github.com>
2021-10-09 02:58:46 -0400

    Fix GroobyVR scraper

--- pkg/scrape/groobyvr.go
+++ pkg/scrape/groobyvr.go
@@ -26,27 +26,37 @@ func GroobyVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out cha
 	sceneCollector.OnHTML(`html`, func(e *colly.HTMLElement) {
 		sc := models.ScrapedScene{}
 		sc.SceneType = "VR"
+		sc.Studio = "GroobyVR"
 		sc.Site = siteID
 		sc.HomepageURL = strings.Split(e.Request.URL.String(), "?")[0]
-		sc.Studio = "GroobyVR"
-		sc.Title = strings.TrimSpace(e.ChildText("span.settitle"))
-		sc.Cast = append(sc.Cast, e.ChildText("span.modellink"))
 
-		coverURL := e.Request.AbsoluteURL(e.ChildAttr("div.bigvideo div.videohere img", "src"))
+		// Title
+		sc.Title = strings.Replace(e.ChildText(`title`), "Grooby VR: ", "", -1)
+
+		// Cast
+		e.ForEach(`div.trailer_toptitle_left a`, func(id int, e *colly.HTMLElement) {
+			sc.Cast = append(sc.Cast, strings.TrimSpace(e.Text))
+		})
+
+		// Cover URL
+		coverURL := "https:" + e.Request.AbsoluteURL(e.ChildAttr("div.player-thumb img", "src"))
 		sc.Covers = append(sc.Covers, coverURL)
 
+		// Scene ID - get from URL
 		tmps := strings.Split(coverURL, "/")
 		tmp := strings.Replace(tmps[len(tmps)-1], ".jpg", "", -1)
 		sc.SiteID = tmp
 		sc.SceneID = slugify.Slugify(sc.Site) + "-" + sc.SiteID
 
-		sc.Synopsis = strings.TrimSpace(e.ChildText(`div.set_meta p:not([class])`))
+		// Synopsis
+		sc.Synopsis = strings.TrimSpace(e.ChildText(`div.trailerblock p`))
 
-		dateString := strings.Replace(e.ChildText(`p.set_meta_details`), "Added: ", "", -1)
-		tmpDate, _ := goment.New(dateString, "Do MMMM YYYY")
+		// Date
+		dateString := strings.Replace(e.ChildText(`div.set_meta`), "Added ", "", -1)
+		tmpDate, _ := goment.New(dateString, "MMMM D, YYYY")
 		sc.Released = tmpDate.Format("YYYY-MM-DD")
 
-		// not every scene has a vod link
+		// Pull data from vod page - not every scene has a vod link
 		ctx := colly.NewContext()
 		ctx.Put("scene", &sc)
 		vodURL := e.ChildAttr("a.downBtnbuy", "href")
@@ -60,19 +70,32 @@ func GroobyVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out cha
 	vodCollector.OnHTML(`html`, func(e *colly.HTMLElement) {
 		sc := e.Request.Ctx.GetAny("scene").(*models.ScrapedScene)
 
+		// Gallery
 		sc.Gallery = e.ChildAttrs("div.gallery-group img", "data-orig-file")
 
-		// not every vod page has a duration listed
-		tmpDuration := strings.Replace(e.ChildText(`ul li:contains(length)`), "Length: ", "", -1)
+		// Duration - not every vod page has a duration listed
+		r := strings.NewReplacer("Length: ", "", "Length ", "")
+		tmpDuration := r.Replace(e.ChildText(`ul li:contains(length)`))
 		tmpDuration = strings.Split(tmpDuration, "m")[0]
 		duration, err := strconv.Atoi(tmpDuration)
 		if err == nil && duration > 0 {
 			sc.Duration = duration
 		}
+
+		// Tags
+		e.ForEach(`span.meta-tag a`, func(id int, e *colly.HTMLElement) {
+			sc.Tags = append(sc.Tags, strings.TrimSpace(e.Text))
+		})
+
+		// Not every page has tags so use categories as well
+		e.ForEach(`span.meta-cat a`, func(id int, e *colly.HTMLElement) {
+			sc.Tags = append(sc.Tags, strings.TrimSpace(e.Text))
+		})
+
 	})
 
-	siteCollector.OnHTML(`div.frontpage_sexyvideo h4 a`, func(e *colly.HTMLElement) {
-		sceneURL := e.Request.AbsoluteURL(e.Attr("href"))
+	siteCollector.OnHTML(`div.videohere a`, func(e *colly.HTMLElement) {
+		sceneURL := "https:" + e.Request.AbsoluteURL(e.Attr("href"))
 
 		if !funk.ContainsString(knownScenes, sceneURL) {
 			sceneCollector.Visit(sceneURL)
@@ -84,7 +107,7 @@ func GroobyVR(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out cha
 		siteCollector.Visit(pageURL)
 	})
 
-	siteCollector.Visit("https://groobyvr.com/tour/categories/movies/1/latest/")
+	siteCollector.Visit("https://www.groobyvr.com/tour/categories/movies/1/latest/")
 
 	if updateSite {
 		updateSiteLastUpdate(scraperID)
