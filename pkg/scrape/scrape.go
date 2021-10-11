a1097752b58965297c158964a1e0494b2db66c56
alex <85208095+alexnsfw@users.noreply.github.com>
2021-10-11 14:44:49 -0400

    Fix VRBangers API calls

--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -19,13 +19,13 @@ import (
 
 var log = &common.Log
 
-var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
 
 func createCollector(domains ...string) *colly.Collector {
 	c := colly.NewCollector(
 		colly.AllowedDomains(domains...),
 		colly.CacheDir(getScrapeCacheDir()),
-		colly.UserAgent(userAgent),
+		colly.UserAgent(UserAgent),
 	)
 
 	c = createCallbacks(c)
