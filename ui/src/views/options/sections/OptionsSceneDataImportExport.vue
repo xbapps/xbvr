de7910cc37d78dde7b206fd32b178cd2350f4238
NodudeWasTaken <75137537+NodudeWasTaken@users.noreply.github.com>
2022-01-24 12:47:49 +0100

    Removed custom scene ui entry in Data Import/Export section

--- ui/src/views/options/sections/OptionsSceneDataImportExport.vue
+++ ui/src/views/options/sections/OptionsSceneDataImportExport.vue
@@ -17,24 +17,12 @@
         {{$t('If you already have scraped scene data, you can export it below.')}}
       </p>
       <b-button type="is-primary" @click="exportContent">{{$t('Export content bundle')}}</b-button>
-      <hr/>
-    </div>
-    <div class="content">
-      <h3>{{$t("Add custom scene")}}</h3>
-      <p>
-        {{$t("You can add a custom scene with a specific title.")}}
-      </p>
-      <b-field grouped>
-        <b-input v-model="sceneTitle" :placeholder="$t('Scene title')" type="search" icon="plus"></b-input>
-        <div class="button is-button is-primary" v-on:click="addScene">{{$t('Add custom scene')}}</div>
-      </b-field>
     </div>
   </div>
 </template>
 
 <script>
 import ky from 'ky'
-
 export default {
   name: 'OptionsSceneDataImportExport',
   data () {
@@ -50,12 +38,7 @@ export default {
     },
     exportContent () {
       ky.get('/api/task/bundle/export')
-    },
-	addScene() {
-      if (this.sceneTitle !== '') {
-        ky.post('/api/scene/create', { searchParams: { title: this.sceneTitle }, json: {} })
-      }
-	}
+    }
   }
 }
 </script>
