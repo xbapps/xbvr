d1476c2a60a2fc93a901bf3c91c6e232a57fe2c6
theRealKLH <65736720+theRealKLH@users.noreply.github.com>
2022-07-04 11:15:28 -0400

    fe190

--- pkg/api/deovr.go
+++ pkg/api/deovr.go
@@ -403,6 +403,7 @@ func (i DeoVRResource) getDeoScene(req *restful.Request, resp *restful.Response)
 		if videoFiles[0].VideoProjection == "mkx200" ||
 			videoFiles[0].VideoProjection == "mkx220" ||
 			videoFiles[0].VideoProjection == "rf52" ||
+			videoFiles[0].VideoProjection == "fisheye190" ||
 			videoFiles[0].VideoProjection == "vrca220" {
 			stereoMode = "sbs"
 			screenType = videoFiles[0].VideoProjection
