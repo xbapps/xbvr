7cd3aab7858034f431e23e8b7d06c1ec8752ae7b
toshski <toshski8760@gmail.com>
2022-11-15 19:35:13 +1300

    Disable Trailer Playlist from SLR

--- pkg/scrape/slrstudios.go
+++ pkg/scrape/slrstudios.go
@@ -1,7 +1,6 @@
 package scrape
 
 import (
-	"encoding/json"
 	"html"
 	"regexp"
 	"strconv"
@@ -92,16 +91,16 @@ func SexLikeReal(wg *sync.WaitGroup, updateSite bool, knownScenes []string, out 
 		// Duration
 		sc.Duration = e.Request.Ctx.GetAny("duration").(int)
 
-		// trailer details
-		sc.TrailerType = "scrape_json"
-		jsonRequest := models.TrailerScrape{SceneUrl: sc.HomepageURL, HtmlElement: "script[type=\"text/javascript\"]", ExtractRegex: "videoData:\\s*(.*}),",
-			RecordPath: "src", ContentPath: "url", EncodingPath: "encoding", QualityPath: "quality", ContentBaseUrl: ""}
-		log.Infof("%v", jsonRequest)
-		jsonStr, _ := json.Marshal(jsonRequest)
-		sc.TrailerSrc = string(jsonStr)
+		// // trailer details
+		// sc.TrailerType = "scrape_json"
+		// jsonRequest := models.TrailerScrape{SceneUrl: sc.HomepageURL, HtmlElement: "script[type=\"text/javascript\"]", ExtractRegex: "videoData:\\s*(.*}),",
+		// 	RecordPath: "src", ContentPath: "url", EncodingPath: "encoding", QualityPath: "quality", ContentBaseUrl: ""}
+		// log.Infof("%v", jsonRequest)
+		// jsonStr, _ := json.Marshal(jsonRequest)
+		// sc.TrailerSrc = string(jsonStr)
 
-		sc.TrailerType = "slr"
-		sc.TrailerSrc = "https://api.sexlikereal.com/virtualreality/video/id/" + sc.SiteID
+		// sc.TrailerType = "slr"
+		// sc.TrailerSrc = "https://api.sexlikereal.com/virtualreality/video/id/" + sc.SiteID
 
 		// Extract from JSON meta data
 		// NOTE: SLR only provides certain information like duration as json metadata inside a script element
