590e6499dfbe9630d46fac3bfd6036152a0db78b
crwxaj <52156245+crwxaj@users.noreply.github.com>
2022-11-21 21:04:18 +0100

    fix cron.go

--- pkg/server/cron.go
+++ pkg/server/cron.go
@@ -37,13 +37,13 @@ func SetupCron() {
 	go tasks.CalculateCacheSizes()
 
 	if config.Config.Cron.RescrapeSchedule.RunAtStartDelay > 0 {
-		time.AfterFunc(time.Duration(config.Config.Cron.RescrapeSchedule.RunAtStartDelay*int(time.Minute)), scrapeCron)
+		time.AfterFunc(time.Duration(config.Config.Cron.RescrapeSchedule.RunAtStartDelay)*time.Minute, scrapeCron)
 	}
 	if config.Config.Cron.RescanSchedule.RunAtStartDelay > 0 {
-		time.AfterFunc(time.Duration(config.Config.Cron.RescanSchedule.RunAtStartDelay*int(time.Minute)), rescanCron)
+		time.AfterFunc(time.Duration(config.Config.Cron.RescanSchedule.RunAtStartDelay)*time.Minute, rescanCron)
 	}
 	if config.Config.Cron.PreviewSchedule.RunAtStartDelay > 0 {
-		time.AfterFunc(time.Duration(config.Config.Cron.PreviewSchedule.RunAtStartDelay*int(time.Minute)), generatePreviewCron)
+		time.AfterFunc(time.Duration(config.Config.Cron.PreviewSchedule.RunAtStartDelay)*time.Minute, generatePreviewCron)
 	}
 
 	log.Println(fmt.Sprintf("Next Rescrape Task at %v", cronInstance.Entry(rescrapTask).Next))
