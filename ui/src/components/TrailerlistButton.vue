538a2cac966af3adfb01b0439fa710fd7c7b5630
vt-idiot <81622808+vt-idiot@users.noreply.github.com>
2022-12-26 08:25:21 -0500

    Fix Trailerlist Button Tooltip
    The tooltip currently changes with the item's watchlist status, not the trailerlist status

--- ui/src/components/TrailerlistButton.vue
+++ ui/src/components/TrailerlistButton.vue
@@ -2,7 +2,7 @@
   <a :class="buttonClass"
    v-if="!item.is_available & item.trailer_source !=='' & this.$store.state.optionsWeb.web.sceneTrailerlist"
      @click="$store.commit('sceneList/toggleSceneList', {scene_id: item.scene_id, list: 'trailerlist'})"
-     :title="item.watchlist ? 'Remove from Trailer List' : 'Add to Trailer List'">
+     :title="item.trailerlist ? 'Remove from Trailer List' : 'Add to Trailer List'">
     <b-icon pack="mdi" :icon="item.watchlist ? 'movie-search-outline' : 'movie-search-outline'" size="is-small"/>
   </a>
 </template>
