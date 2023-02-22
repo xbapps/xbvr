97591707c6707363aa5e2c74b2eeae361cc82087
toshski <104477758+toshski@users.noreply.github.com>
2023-02-23 01:13:23 +1300

    fix: Toggle Buttons of Scenes from Quick Find didn't update (#1149)
    Fixes #1116

--- ui/src/components/WatchlistButton.vue
+++ ui/src/components/WatchlistButton.vue
@@ -1,6 +1,6 @@
 <template>
   <a :class="buttonClass"
-     @click="$store.commit('sceneList/toggleSceneList', {scene_id: item.scene_id, list: 'watchlist'})"
+     @click="toggleState()"
      :title="item.watchlist ? 'Remove from watchlist' : 'Add to watchlist'">
     <b-icon pack="mdi" :icon="item.watchlist ? 'calendar-check' : 'calendar-blank'" size="is-small"/>
   </a>
@@ -16,6 +16,13 @@ export default {
         return 'button is-primary is-small'
       }
       return 'button is-primary is-outlined is-small'
+    }
+  },
+  methods: {
+    toggleState() {
+      let currentToggle=this.item.watchlist
+      this.$store.commit('sceneList/toggleSceneList', {scene_id: this.item.scene_id, list: 'watchlist'})
+      this.item.watchlist=!currentToggle
     }
   }
 }
