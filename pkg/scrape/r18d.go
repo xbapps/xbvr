d8c17d92f2abeec45847f1e340b37b88bd49e8d4
vt-idiot <81622808+vt-idiot@users.noreply.github.com>
2023-10-20 16:30:22 -0400

    Update r18d.go
    Removed one set of filler filenames.

--- pkg/scrape/r18d.go
+++ pkg/scrape/r18d.go
@@ -106,7 +106,7 @@ func ScrapeR18D(out *[]models.ScrapedScene, queryString string) error {
 		// Filler Filenames
 		resolutions := []string{"vmb"}
 		if quality == "HQ" {
-			resolutions = []string{"vrv1uhqe", "vrv1uhqf"}
+			resolutions = []string{"vrv1uhqe"}
 		} else if quality == "8K" {
 			resolutions = []string{"vrv1uhqf", "vrv18khia"}
 		}
