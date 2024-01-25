367a2185b7095b1b899765ea672510ebee72a2a1
Asparghus <147214977+Asparghus@users.noreply.github.com>
2024-01-25 13:27:29 +0000

    Minor Typo Serach/Search

--- ui/src/views/scenes/Details.vue
+++ ui/src/views/scenes/Details.vue
@@ -105,7 +105,7 @@
                   </div>
                   <div class="column pt-0">
                     <div class="is-flex is-pulled-right" style="gap: 0.25rem">
-                      <a class="button is-primary is-outlined is-small" @click="searchAlternateSourceScene()" title="Serach for a different scene" v-if="displayingAlternateSource">
+                      <a class="button is-primary is-outlined is-small" @click="searchAlternateSourceScene()" title="Search for a different scene" v-if="displayingAlternateSource">
                         <b-icon pack="mdi" icon="movie-search-outline" size="is-small"/>
                       </a>
                       <a class="button is-primary is-outlined is-small" @click="scrapeScene()" title="Scrape and create an XBVR scene (not a link)" v-if="displayingAlternateSource">
