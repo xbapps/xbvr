5f3585012c3e840c7ba0c6920f439abc8f2d184a
balckpixie <kuropixie@gmail.com>
2024-03-20 13:12:05 +0000

    サムネイル生成タスク呼び出し追加

--- pkg/api/tasks.go
+++ pkg/api/tasks.go
@@ -81,6 +81,9 @@ func (i TaskResource) WebService() *restful.WebService {
 	ws.Route(ws.GET("/preview/generate").To(i.previewGenerate).
 		Metadata(restfulspec.KeyOpenAPITags, tags))
 
+	ws.Route(ws.GET("/thumbnail/generate").To(i.thumbnailGenerate).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+
 	ws.Route(ws.GET("/funscript/export-all").To(i.exportAllFunscripts).
 		Metadata(restfulspec.KeyOpenAPITags, tags))
 
@@ -203,6 +206,10 @@ func (i TaskResource) restoreBundle(req *restful.Request, resp *restful.Response
 
 func (i TaskResource) previewGenerate(req *restful.Request, resp *restful.Response) {
 	go tasks.GeneratePreviews(nil)
+}
+
+func (i TaskResource) thumbnailGenerate(req *restful.Request, resp *restful.Response) {
+	go tasks.GenerateThumnbnails(nil)
 }
 
 func (i TaskResource) scrapeJAVR(req *restful.Request, resp *restful.Response) {
