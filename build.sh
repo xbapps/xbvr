8ad45d2070282ec3246c620503ad3e84a08f2da9
balckpixie <kuropixie@gmail.com>
2024-03-21 09:05:47 +0000

    build用

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-yarn build
-go generate
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC="x86_64-w64-mingw32-gcc" CXX="x86_64-w64-mingw32-g++" go build -ldflags "-H windowsgui" -tags=json1 -o xbvr.exe pkg/tray/main.go
