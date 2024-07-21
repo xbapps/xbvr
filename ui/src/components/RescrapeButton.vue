0e46b4c15ebbcb3372bb12c95912634d567dd742
Asparghus <147214977+Asparghus@users.noreply.github.com>
2024-07-22 00:03:56 +0100

    Update RescrapeButton.vue
    Updated the Rescrape Scene function to work with scenes that have scrape_id and belong to a configured scraper. By toggling it's needs_update and running that scraper.
    
    It can be useful in case of single scenes without images.
    
    I've had to add a small delay (200ms) as it was not triggering the forceUpdate when making the API call, even when checking if this.item.needs_update was true.

--- ui/src/components/RescrapeButton.vue
+++ ui/src/components/RescrapeButton.vue
@@ -1,7 +1,7 @@
 <template>
   <a class="button is-dark is-outlined is-small"
-    @click="rescrapeSingleScene()"
-    :title="'Rescrape scene'">
+    @click="rescrapeScene()"
+    :title="'Rescrape Scene'">
     <b-icon pack="mdi" icon="web-refresh" size="is-small"/>
   </a>
 </template>
@@ -12,42 +12,51 @@ export default {
   name: 'RescrapeButton',
   props: { item: Object },
   methods: {
-    async rescrapeSingleScene () {
+    delay(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    },
+    async rescrapeScene () {
       let site = ""
 
-      if (this.item.scene_url.toLowerCase().includes("dmm.co.jp")) {
-        ky.post('/api/task/scrape-javr', { json: { s: "r18d", q: this.item.scene_id } })
+      this.$store.commit('sceneList/toggleSceneList', {scene_id: this.item.scene_id, list: 'needs_update'})
+      if (this.item.scraper_id && this.item.needs_update) {
+        await this.delay(200);
+        ky.get(`/api/task/scrape?site=${this.item.scraper_id}`)
       } else {
+        if (this.item.scene_url.toLowerCase().includes("dmm.co.jp")) {
+          ky.post('/api/task/scrape-javr', { json: { s: "r18d", q: this.item.scene_id } })
+        } else {
 
-        const sites = await ky.get('/api/options/sites').json()
-        console.info(sites)
+          const sites = await ky.get('/api/options/sites').json()
+          console.info(sites)
 
-        for (const element of sites) {
-          if (this.item.scene_url.toLowerCase().includes(element.id)) {
-            site = element.id
+          for (const element of sites) {
+            if (this.item.scene_url.toLowerCase().includes(element.id)) {
+              site = element.id
+            }
           }
-        }
 
-        if (this.item.scene_url.toLowerCase().includes("sexlikereal.com")) {
-          site = "slr-single_scene"
+          if (this.item.scene_url.toLowerCase().includes("sexlikereal.com")) {
+            site = "slr-single_scene"
+          }
+          if (this.item.scene_url.toLowerCase().includes("czechvrnetwork.com")) {
+            site = "czechvr-single_scene"
+          }
+          if (this.item.scene_url.toLowerCase().includes("povr.com")) {
+            site = "povr-single_scene"
+          }
+          if (this.item.scene_url.toLowerCase().includes("vrporn.com")) {
+            site = "vrporn-single_scene"
+          }
+          if (this.item.scene_url.toLowerCase().includes("vrphub.com")) {
+            site = "vrphub-single_scene"
+          }
+          if (site == "") {
+            this.$buefy.toast.open({message: `No scrapers exist for this domain`, type: 'is-danger', duration: 5000})      
+            return
+          }    
+          ky.post(`/api/task/singlescrape`, {timeout: false, json: { site: site, sceneurl: this.item.scene_url, additionalinfo:[] }})
         }
-        if (this.item.scene_url.toLowerCase().includes("czechvrnetwork.com")) {
-          site = "czechvr-single_scene"
-        }
-        if (this.item.scene_url.toLowerCase().includes("povr.com")) {
-          site = "povr-single_scene"
-        }
-        if (this.item.scene_url.toLowerCase().includes("vrporn.com")) {
-          site = "vrporn-single_scene"
-        }
-        if (this.item.scene_url.toLowerCase().includes("vrphub.com")) {
-          site = "vrphub-single_scene"
-        }
-        if (site == "") {
-          this.$buefy.toast.open({message: `No scrapers exist for this domain`, type: 'is-danger', duration: 5000})      
-          return
-        }    
-        ky.post(`/api/task/singlescrape`, {timeout: false, json: { site: site, sceneurl: this.item.scene_url, additionalinfo:[] }})
       }
     }
   }
