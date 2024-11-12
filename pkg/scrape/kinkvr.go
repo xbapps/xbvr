04531a349dbc6fb2ab1712e21a83b1ad30bb826f
pops64 <poptrek@gmail.com>
2024-11-12 12:05:45 -0500

    Spin off kinkvr completely
    After some digging it appears kinkvr was sold and not a website redesign by badoink. Now completly separates kinkvr from badoink

--- pkg/scrape/kinkvr.go
+++ pkg/scrape/kinkvr.go
@@ -10,12 +10,14 @@ import (
 	"github.com/xbapps/xbvr/pkg/models"
 )
 
-func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene, singleSceneURL string, scraperID string, siteID string, domain string, singeScrapeAdditionalInfo string, limitScraping bool) error {
+func KinkVR(wg *models.ScrapeWG, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene, singleSceneURL string, singeScrapeAdditionalInfo string, limitScraping bool) error {
 	defer wg.Done()
+	scraperID := "kinkvr"
+	siteID := "KinkVR"
 	logScrapeStart(scraperID, siteID)
 
-	sceneCollector := createCollector(domain)
-	siteCollector := createCollector(domain)
+	sceneCollector := createCollector("kinkvr.com")
+	siteCollector := createCollector("kinkvr.com")
 
 	// These cookies are needed for age verification.
 	siteCollector.OnRequest(func(r *colly.Request) {
@@ -33,7 +35,7 @@ func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, o
 		sc.Studio = "Badoink"
 		sc.Site = siteID
 		sc.SiteID = ""
-		sc.HomepageURL, _ = strings.CutSuffix(e.Request.URL.String(), "/")
+		sc.HomepageURL = e.Request.URL.String()
 
 		// Cover Url
 		coverURL := e.ChildAttr("div#povVideoContainer dl8-video", "poster")
@@ -78,7 +80,8 @@ func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, o
 		sc.Title = e.ChildText("h1.page-title")
 
 		// Scene ID -- Uses the ending number of the video url instead of the ID used for the directory that the video link is stored in(Maintains backwards compatibility with old scenes)
-		tmp := strings.Split(sc.HomepageURL, "/")
+		tmpUrlStr, _ := strings.CutSuffix(e.Request.URL.String(), "/")
+		tmp := strings.Split(tmpUrlStr, "/")
 		siteIDstr := strings.Split(tmp[len(tmp)-1], "-")
 		sc.SiteID = siteIDstr[len(siteIDstr)-1]
 
@@ -98,7 +101,7 @@ func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, o
 	})
 
 	siteCollector.OnHTML(`div.video-grid-view a`, func(e *colly.HTMLElement) {
-		sceneURL, _ := strings.CutSuffix(e.Request.AbsoluteURL(e.Attr("href")), "/")
+		sceneURL := e.Request.AbsoluteURL(e.Attr("href"))
 		// If scene exist in database, there's no need to scrape
 		if !funk.ContainsString(knownScenes, sceneURL) {
 			sceneCollector.Visit(sceneURL)
@@ -108,7 +111,7 @@ func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, o
 	if singleSceneURL != "" {
 		sceneCollector.Visit(singleSceneURL)
 	} else {
-		siteCollector.Visit("https://" + domain + "/videos/page1")
+		siteCollector.Visit("https://kinkvr.com/videos/page1")
 	}
 
 	if updateSite {
@@ -116,10 +119,6 @@ func BadoinkSitev2(wg *models.ScrapeWG, updateSite bool, knownScenes []string, o
 	}
 	logScrapeFinished(scraperID, siteID)
 	return nil
-}
-
-func KinkVR(wg *models.ScrapeWG, updateSite bool, knownScenes []string, out chan<- models.ScrapedScene, singleSceneURL string, singeScrapeAdditionalInfo string, limitScraping bool) error {
-	return BadoinkSitev2(wg, updateSite, knownScenes, out, singleSceneURL, "kinkvr", "KinkVR", "kinkvr.com", singeScrapeAdditionalInfo, limitScraping)
 }
 
 func init() {
