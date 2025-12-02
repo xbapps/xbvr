fcd9162e2d34d7f3d1ac631ad460d03485273b8d
toshski <toshski8760@gmail.com>
2025-12-02 13:23:41 +1300

    Fix HSP not displaying thumbnails from myfiles

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	auth "github.com/abbot/go-http-auth"
@@ -141,6 +142,8 @@ func StartServer(version, commit, branch, date string) {
 	// If the client request has a cache-control header (such as 'no-cache'), pass them
 	// onto the imageproxy so that this can be respected.
 	p.PassRequestHeaders = append(p.PassRequestHeaders, "Cache-Control")
+	u, _ := url.Parse("http://127.0.0.1:" + strconv.Itoa(config.Config.Server.Port))
+	p.DefaultBaseURL = u
 	r.PathPrefix("/img/").Handler(ForceShortCacheHandler(http.StripPrefix("/img", p)))
 	hmp := NewHeatmapThumbnailProxy(p, diskCache(filepath.Join(common.AppDir, "heatmapthumbnailproxy")))
 	r.PathPrefix("/imghm/").Handler(http.StripPrefix("/imghm", hmp))
