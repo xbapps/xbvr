b6ef5da08f343d21e147408ec15043882c013a39
Alderamin <126297468+Alderamin49ly@users.noreply.github.com>
2026-09-14 14:03:48 -0400

    scraper: fix: RealJamVR and PornCornVR scrapers (#2240)
    Old Chrome version in user agent resulted in 403 Forbidden errors
    
    Co-authored-by: Alderamin <alderamin49ly@gmail.com>

--- pkg/scrape/scrape.go
+++ pkg/scrape/scrape.go
@@ -20,7 +20,7 @@ import (
 
 var log = &common.Log
 
-var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
 
 func createCollector(domains ...string) *colly.Collector {
 	c := colly.NewCollector(
